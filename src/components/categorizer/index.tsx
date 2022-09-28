f02e0c0f4eac8ed466a95f0bffa522cfd6897578
mkamal <mkamalbasha@informatica.com>
2022-09-28 23:24:02 +0530

    Introduced Wizard in Categorizer

--- src/components/categorizer/index.tsx
+++ src/components/categorizer/index.tsx
@@ -1,7 +1,8 @@
 import {
-    Button, Radio, Step, StepLabel, Stepper, Typography,
+    Button, Step, StepLabel, Stepper, Typography,
 } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import ReactJson from 'react-json-view';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -17,10 +18,26 @@ import { TransactionType, TransData } from '../../services/redux/transactions/tr
 import CategorizeCash from './categorize-cash';
 import CategorizeOnline from './categorize-online';
 import { TransCategory } from '../../utils/trans-category';
+import './categorizer.css';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            width: '100%',
+        },
+        button: {
+            marginRight: theme.spacing(1),
+        },
+        instructions: {
+            marginTop: theme.spacing(1),
+            marginBottom: theme.spacing(1),
+        },
+    }),
+);
 
 function Categorizer() {
     const [message, setMessage] = useState<string>();
-    const [type, setType] = useState<'online' | 'cash' | undefined>('cash');
+    const [type, setType] = useState<'online' | 'cash' | 'monthly' | undefined>('cash');
     const [secret, setSecret] = useState('');
     const dispatch = useDispatch();
     const sheetData: GoogleSheet = useSelector((state: any) => state.sheet);
@@ -63,81 +80,132 @@ function Categorizer() {
         });
     }, [monthlyMaintSplit]);
     const [activeStep, setActiveStep] = useState(0);
-    const steps = useMemo(() => (['Cash Transactions', 'Online Transactions', 'Calculated Mappings']), []);
+    const steps = useMemo(() => (
+        ['Cash Transactions', 'Online Transactions', 'Map Transactions', 'Categorize Maintenance']), []);
     const isStepOptional = useCallback((index: number) => false, []);
     const isCompletedStep = useCallback((index: number) => false, []);
+    const classes = useStyles();
+    const handleBack = useCallback(() => {
+        switch (activeStep) {
+        case 3: {
+            setType(undefined);
+            setActiveStep(2);
+            break;
+        }
+        case 2: {
+            setType('online');
+            setActiveStep(1);
+            break;
+        }
+        case 1: {
+            setType('cash');
+            setActiveStep(0);
+            break;
+        }
+        default:
+            // Nothing to be done
+        }
+    }, [activeStep]);
+    const handleSkip = useCallback(() => {}, []);
+    const handleNext = useCallback(() => {
+        switch (activeStep) {
+        case 0: {
+            setType('online');
+            setActiveStep(1);
+            break;
+        }
+        case 1: {
+            setType(undefined);
+            setActiveStep(2);
+            splitMonthly();
+            break;
+        }
+        case 2: {
+            setType(undefined);
+            setActiveStep(3);
+            splitMonthlyMaintenance();
+            break;
+        }
+        default:
+            // Nothing to be done
+        }
+    }, [activeStep, splitMonthlyMaintenance, splitMonthly]);
     return (
         <div>
             {!!message && <Alert severity="info">{message}</Alert> }
             {!!initError && <Alert severity="error">{initError}</Alert>}
-            <h2>Categorize Transactions</h2>
-            <Stepper activeStep={activeStep}>
-                {steps.map((label, index) => {
-                    const stepProps: { completed?: boolean } = {};
-                    const labelProps: { optional?: React.ReactNode } = {};
-                    if (isStepOptional(index)) {
-                        labelProps.optional = <Typography variant="caption">Optional</Typography>;
-                    }
-                    if (isCompletedStep(index)) {
-                        stepProps.completed = false;
-                    }
-                    return (
-                        <Step key={label} {...stepProps}>
-                            <StepLabel {...labelProps}>{label}</StepLabel>
-                        </Step>
-                    );
-                })}
-            </Stepper>
-            <div>
-                <Radio
-                    checked={type === 'cash'}
-                    onChange={() => setType('cash')}
-                    value="cash"
-                    name="radio-buttons"
-                    inputProps={{ 'aria-label': 'Cash' }}
-                /> Cash &nbsp;&nbsp;&nbsp;
-                <Radio
-                    checked={type === 'online'}
-                    onChange={() => setType('online')}
-                    value="online"
-                    name="radio-buttons"
-                    inputProps={{ 'aria-label': 'Online' }}
-                /> Online &nbsp;&nbsp;&nbsp;
-                <Button
-                    variant="outlined"
-                    disabled={!cashTransData || !onlineTransData}
-                    onClick={splitMonthly}
-                >
-                    Categorize Transactions Monthly
-                </Button>&nbsp;&nbsp;&nbsp;
-                <Button
-                    variant="outlined"
-                    disabled={!monthlyCatSplit}
-                    onClick={saveCategorized}
-                >
-                    Save Categorized Transactions
-                </Button>&nbsp;&nbsp;&nbsp;
-                <Button
-                    variant="outlined"
-                    disabled={!cashTransData || !onlineTransData}
-                    onClick={splitMonthlyMaintenance}
-                >
-                    Categorize Maintenance Monthly
-                </Button>&nbsp;&nbsp;&nbsp;
-                <Button
-                    variant="outlined"
-                    disabled={!monthlyMaintSplit}
-                    onClick={saveMaitenenceSplit}
-                >
-                    Save Categorized Maintenance
-                </Button>
+            <div className="stepper-header">
+                <Stepper className="steps" activeStep={activeStep}>
+                    {steps.map((label, index) => {
+                        const stepProps: { completed?: boolean } = {};
+                        const labelProps: { optional?: React.ReactNode } = {};
+                        if (isStepOptional(index)) {
+                            labelProps.optional = <Typography variant="caption">Optional</Typography>;
+                        }
+                        if (isCompletedStep(index)) {
+                            stepProps.completed = false;
+                        }
+                        return (
+                            <Step key={label} {...stepProps}>
+                                <StepLabel {...labelProps}>{label}</StepLabel>
+                            </Step>
+                        );
+                    })}
+                </Stepper>
+                <div className="nav">
+                    <Button
+                        disabled={activeStep === 0}
+                        onClick={handleBack}
+                        className={classes.button}
+                    >
+                        Back
+                    </Button>
+                    {isStepOptional(activeStep) && (
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleSkip}
+                            className={classes.button}
+                        >
+                            Skip
+                        </Button>
+                    )}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleNext}
+                        className={classes.button}
+                    >
+                        {(activeStep === steps.length - 1 && 'Save') || 'Next'}
+                    </Button>
+                </div>
+            </div>
+            <div className="control">
+                {activeStep === 2 && (
+                    <Button
+                        variant="outlined"
+                        disabled={!monthlyCatSplit}
+                        onClick={saveCategorized}
+                    >
+                        Save Categorized
+                    </Button>
+                )}&nbsp;&nbsp;&nbsp;
+                {activeStep === 3 && (
+                    <Button
+                        variant="outlined"
+                        disabled={!monthlyMaintSplit}
+                        onClick={saveMaitenenceSplit}
+                    >
+                        Save Split
+                    </Button>
+                )}
             </div>
             {!initError && !!sheetData.sheetConfig.secret && (
                 <>
-                    {type === 'cash' && <CategorizeCash />}
-                    {type === 'online' && <CategorizeOnline />}
-                    {!type && !!monthlyMaintSplit && <ReactJson src={monthlyMaintSplit} />}
-                    {!type && !!monthlyCatSplit && <ReactJson src={monthlyCatSplit} />}
+                    {activeStep === 0 && <CategorizeCash />}
+                    {activeStep === 1 && <CategorizeOnline />}
+                    {activeStep === 2 && !!monthlyCatSplit && <ReactJson src={monthlyCatSplit} />}
+                    {activeStep === 3 && !!monthlyMaintSplit && <ReactJson src={monthlyMaintSplit} />}
                 </>
             )}
         </div>
