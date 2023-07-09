391bbbd6ebf98fa80e4b25d273777b7926b91288
akhadir <abk0680@yahoo.com>
2023-07-09 21:45:03 +0530

    Change in Query

--- src/services/googleapis/drive-util.ts
+++ src/services/googleapis/drive-util.ts
@@ -3,12 +3,14 @@ import { appConfig } from '..';
 
 const folderId = '1zVRwXWZjfAS3Hk1rXq-XtqurPcQ-yAW-'; // bills directory
 
+// eslint-disable-next-line max-len
+const query = encodeURI(`'${folderId}' in parents and (mimeType = 'image/jpeg' or mimeType = 'image/png' or mimeType = 'application/pdf' or mimeType = 'application/vnd.google-apps.folder')`);
 const apiKey = appConfig.googleAPIKey;
 
 export default async function fetchFilesFromFolder() {
     try {
         const response = await axios.get(
-            `https://www.googleapis.com/drive/v3/files?q='${folderId}'%20in%20parents&key=${apiKey}`,
+            `https://www.googleapis.com/drive/v3/files?q=${query}&key=${apiKey}`,
         );
 
         const { files } = response.data;
